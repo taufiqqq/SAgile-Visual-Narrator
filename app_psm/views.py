5c712c1bc1acd218db5230c153f3098584250aad
MeorIdlan <75961990+MeorIdlan@users.noreply.github.com>
2023-07-09 23:15:23 +0800

    Update views.py
    Accepts post method only

--- app_psm/views.py
+++ app_psm/views.py
@@ -1,13 +1,20 @@
 from api.boundary import Boundary
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
 def ucd_request(request):
-    boundary = Boundary()
-    
-    response = boundary.check_request(request)
-    
-    if response is None:
-        boundary.create_response()
-        return boundary.get_response()
+    if request.method == 'POST':
+        boundary = Boundary()
+        
+        response = boundary.check_request(request)
+        
+        if response is None:
+            boundary.create_response()
+            return boundary.get_response()
+        else:
+            return response
     else:
-        return response
+        response_data = {"message": "Invalid request method"}
+        return JsonResponse(response_data, status=405)
